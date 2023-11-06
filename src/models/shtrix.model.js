7ef6607fbc386f2a09a18c058ee588d7bd52c353
ismailovWD <ismailovWD@gmail.com>
2023-11-06 15:26:07 +0500

    Convert shtrix_kod column string to number in shtrix table

--- src/models/shtrix.model.js
+++ src/models/shtrix.model.js
@@ -2,29 +2,37 @@ const { Model } = require('sequelize');
 const Sequelize = require('sequelize');
 const sequelize = require('../db/db-sequelize');
 const ProductModel = require('./product.model');
-class ShtrixModel extends Model {}
+class ShtrixModel extends Model {
+    toJSON() {
+        let values = Object.assign({}, this.get());
+        if(values.shtrix_kod && typeof values.shtrix_kod == 'string' && values.shtrix_kod != ''){
+            values.shtrix_kod = Number(values.shtrix_kod)
+        }
+        return values;
+    }
+}
 
 ShtrixModel.init({
-    id: { 
-        type: Sequelize.DataTypes.INTEGER, 
-        primaryKey: true, 
-        autoIncrement: true, 
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
         allowNull: false
     },
-    product_id : {
+    product_id: {
         type: Sequelize.DataTypes.INTEGER,
         allowNull: false
     },
-    shtrix_kod : {
+    shtrix_kod: {
         type: Sequelize.DataTypes.STRING(50),
         allowNull: false,
     }
 }, {
-  sequelize,
-  modelName: 'ShtrixModel',
-  tableName: 'shtrix',
-  timestamps: false,
+    sequelize,
+    modelName: 'ShtrixModel',
+    tableName: 'shtrix',
+    timestamps: false,
 });
-ProductModel.hasMany(ShtrixModel, {as: 'shtrix_tables', foreignKey: 'product_id'});
-ShtrixModel.belongsTo(ProductModel, {as: 'product', foreignKey: 'product_id'})
+ProductModel.hasMany(ShtrixModel, { as: 'shtrix_tables', foreignKey: 'product_id' });
+ShtrixModel.belongsTo(ProductModel, { as: 'product', foreignKey: 'product_id' })
 module.exports = ShtrixModel;
